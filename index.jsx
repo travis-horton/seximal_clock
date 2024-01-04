5e0c874c7a2fe6cd973b5f9ba44edba3c6a34363
kiddspazz <travis@travish.com>
2024-01-04 07:46:06 -0700

    Initial commit
    This already has some pretty cool work done in it: I'm making a clock
    built from recursively orbiting circles, and hopefully one whose base
    can be changed.

---
+++ index.jsx
@@ -0,0 +1,98 @@
+import React, { useState, useEffect } from 'react';
+
+import './styles.css';
+
+const sexaHoursInADay = 36;
+const sexaMinsInADay = 36 * sexaHoursInADay;
+const sexaSecsInADay = 36 * sexaMinsInADay;
+
+const milliSecondsInADay = 1000 * 60 * 60 * 24;
+
+export const SeximalTimeKeeping = () => {
+  const [time, setTime] = useState(new Date());
+  const whichHexaSecond = (time.getTime() % milliSecondsInADay) * (
+    sexaSecsInADay / milliSecondsInADay 
+  ) % 36;
+
+  useEffect(() => {
+    const timerID = setInterval( () => tick(), 50);
+
+    return function cleanup() {
+      clearInterval(timerID);
+    };
+  });
+
+  function tick() {
+    setTime(new Date());
+  }
+
+  const degrees = [];
+
+  marks = 36;
+
+  for (let i = 0; i < marks; i++) {
+    degrees.push(i * 10)
+  }
+
+  number_of_labels = 6;
+  clock_labels = [];
+  for (let i = 0; i < number_of_labels; i++) {
+    clock_labels.push(i * (360 / number_of_labels) - 2.5);
+  }
+
+  return (
+    <div>
+      <svg viewBox="0 0 40 40">
+      <circle cx="20" cy="20" r="12" />
+      <g className="marks">
+        {degrees.map((deg, idx) => {
+          if (idx % 6 > 0) {
+          return (
+            <line 
+              key={deg}
+              x1="20"
+              y1="31.5"
+              x2="20"
+              y2="31.25"
+              transform={`rotate(${deg} 20 20)`}
+            />
+          )
+          }
+        })}
+      </g>
+      <circle
+        cx="20"
+        cy="7"
+        r="1"
+        transform={`rotate(${whichHexaSecond * 10} 20 20)`}
+        className="seconds"
+      />
+      <text
+        x="20"
+        y="20"
+        className="time"
+      >
+        :{niftimal[Math.floor(whichHexaSecond)]}
+      </text>
+      <g>
+        {clock_labels.map((deg, idx) => (
+          <text
+            key={deg}
+            x="20"
+            y="10"
+            transform={`rotate(${deg} 20 20)`}
+            className="label"
+          >
+            {idx}
+          </text>
+        ))}
+      </g>
+      </svg>
+    </div>
+  );
+};
+
+const niftimal = [
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+  "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+]
